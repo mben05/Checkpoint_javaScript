8ad8f9f9655d4d905c68f1decb81ef7ab6ea6be0
mben05 <ww.benmarcus@gmail.com>
2023-09-05 17:19:40 +0200

    exercice

---
+++ script.js
@@ -0,0 +1,86 @@
+// function getAbsSum(arr) {
+//   let sum = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     sum = sum + Math.abs(arr[i]);
+//   }
+//   return sum;
+// }
+
+// console.log(getAbsSum([1, 5, -5]));
+
+// function objectToArry(obj) {
+//   let resultTab = [];
+//   for (let i in obj) {
+//     resultTab.push([i, obj[i]]);
+//   }
+//   return resultTab;
+// }
+// console.log(
+//   objectToArry({
+//     D: 1,
+//     B: 2,
+//     C: 3,
+//   })
+// );
+
+// function isSameNum(num1, num2) {
+//   if (num1 === num2) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// console.log(isSameNum(2, 2));
+
+// function greeting(name){
+//   const GUEST_LIST={
+//     Randy:"Germany",
+//     Karla:"France",
+//     Wendy:"Japon",
+//     norman:"England",
+//     sam:"Argentina"
+//   }
+//   for(let i in GUEST_LIST){
+//     is(name==i){
+//       return "Hi I'm" + name + ",i'm from" + "GUEST_LIST[i]+."
+//     }
+
+//   }
+//   return "Hi i'm a guest!"
+// }
+
+// console.log (greeting)
+
+// function getBudgets(tab) {
+//   let sum = 0;
+//   for (let i = 0; i < tab.length; i++);
+//   sum = sum + tab[i].budget;
+//   return sum;
+// }
+
+// console.log(
+//   getBudgets([{ budget: 23000 }, { budget: 40000 }, { budget: 2700 }])
+// );
+
+// function lastNum(tab) {
+//   return tab[tab.length - 1];
+// }
+// console.log(lastNum([5, 2, 1]));
+
+// function afterNYears(objet, n) {
+//   for (i in objet);
+//   return objet[i] + Math.abs(n);
+// }
+
+// console.log(afterNYears({ joel: 24 }, 1));
+
+// function getAbsSum(arr) {
+//   let sum = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     sum = sum + Math.abs(arr[i]);
+//   }
+//   return sum;
+// }
+
+// console.log(getAbsSum([1, 5, -5]));
